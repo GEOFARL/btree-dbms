513df591ac03da4c28c7d2e076bee5ea0543f01d
Maxim <toper.one111@gmail.com>
2023-11-06 22:50:44 +0200

    Implement searching and traversing methods

--- dbms/src/BTreeNode.cpp
+++ dbms/src/BTreeNode.cpp
@@ -10,3 +10,42 @@ BTreeNode::BTreeNode(int _t, bool _leaf)
 
   n = 0;
 }
+
+void BTreeNode::traverse()
+{
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    if (leaf == false)
+    {
+      C[i]->traverse();
+    }
+    cout << " " << keys[i];
+  }
+
+  if (leaf == false)
+  {
+    C[i]->traverse();
+  }
+}
+
+BTreeNode *BTreeNode::search(int k)
+{
+  int i;
+  while (i < n && k > keys[i])
+  {
+    i += 1;
+  }
+
+  if (keys[i] == k)
+  {
+    return this;
+  }
+
+  if (leaf)
+  {
+    return nullptr;
+  }
+
+  return C[i]->search(k);
+}
