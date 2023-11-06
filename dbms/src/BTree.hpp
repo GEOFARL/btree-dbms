0a2f908fdb68262e21d02439daf7bb831f2752d6
Maxim <toper.one111@gmail.com>
2023-11-06 22:54:54 +0200

    Implement BTree class

---
+++ dbms/src/BTree.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include "BTreeNode.hpp"
+
+using namespace std;
+
+class BTree
+{
+  BTreeNode *root;
+  int t;
+
+public:
+  BTree(int _t)
+  {
+    root = nullptr;
+    t = _t;
+  }
+
+  void traverse()
+  {
+    if (root)
+    {
+      root->traverse();
+    }
+  }
+
+  BTreeNode *search(int k)
+  {
+    return (root == nullptr) ? nullptr : root->search(k);
+  }
+};
